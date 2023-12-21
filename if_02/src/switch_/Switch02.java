9677e7ee7e4b8f10c138b70f07089aa198520db5
dkdlxl\bitcamp <https://github.com/jjh319>
2023-12-21 17:43:02 +0900

    java편 switch문 문제풀기

---
+++ if_02/src/switch_/Switch02.java
@@ -0,0 +1,73 @@
+package switch_;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Switch02 {
+
+	
+	public static void main(String[] args) throws IOException { // switch 문제
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("a의 값 : ");
+		int a = sc.nextInt();
+		
+		System.out.print("b의 값 : ");
+		int b = sc.nextInt();
+
+		System.out.print("연산자(+,-,*,/)를 입력 : ");
+		char multi = (char) System.in.read();
+		
+		switch(multi) {
+		
+			case '+' :
+				System.out.println(a + " + " + b + " = " + (a+b) );
+				break;
+			
+			case '-' :
+				System.out.println(a + " - " + b + " = " + (a-b) );
+				break;
+				
+			case '*' :
+				System.out.println(a + " * " + b + " = " + (a*b) );
+				break;
+		
+			case '/' :
+				System.out.println(a + " / " + b + " = " + ( (double) a/b) );
+				break;
+				
+			default :
+				System.out.println("연산자 error");
+				break;
+		} // switch
+		
+	} // main
+
+} // end class
+
+
+/*
+
+	2개의 정수형 숫자와 연산자(+,-,*,/)를 입력하여 계산하시오
+	
+	[실행결과]
+	a의 값 : 25
+	b의 값 : 36
+	연산자(+,-,*,/)를 입력 : +  <- System.in.read() 사용
+	
+	25 + 36 = xx
+	
+	a의 값 : 25
+	b의 값 : 36
+	연산자(+,-,*,/)를 입력 : /
+	
+	25 / 36 = 0.6944444444444444
+	
+	a의 값 : 25
+	b의 값 : 36
+	연산자(+,-,*,/)를 입력 : #
+	
+	연산자 error
+
+*/

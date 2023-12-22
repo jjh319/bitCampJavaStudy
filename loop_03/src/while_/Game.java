f51939be7f5361aea6472cc31afd441b697b7aba
dkdlxl\bitcamp <https://github.com/jjh319>
2023-12-22 15:03:14 +0900

    커밋 문제 확인

--- loop_03/src/while_/Game.java
+++ loop_03/src/while_/Game.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Game {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { // 게임 문제
 		
 		Scanner sc = new Scanner(System.in);
 		

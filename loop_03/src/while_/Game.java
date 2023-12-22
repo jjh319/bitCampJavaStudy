052c34b9d5e6c12a843a0ef6e78a0f953d1fd1b4
jjh319 <https://github.com/jjh319>
2023-12-22 15:16:20 +0900

    커밋 작성자 수정

--- loop_03/src/while_/Game.java
+++ loop_03/src/while_/Game.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Game {
 
 	
-	public static void main(String[] args) { // 게임
+	public static void main(String[] args) { 
 		
 		Scanner sc = new Scanner(System.in);
 		

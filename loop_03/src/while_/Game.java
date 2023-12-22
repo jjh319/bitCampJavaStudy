7dd01fb1798dc4ba2d7fcae9f7b6f7781e53d196
dkdlxl\bitcamp <https://github.com/jjh319>
2023-12-22 15:05:40 +0900

    커밋 작성자 수정

--- loop_03/src/while_/Game.java
+++ loop_03/src/while_/Game.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Game {
 
 	
-	public static void main(String[] args) { // 게임 문제
+	public static void main(String[] args) { // 게임
 		
 		Scanner sc = new Scanner(System.in);
 		

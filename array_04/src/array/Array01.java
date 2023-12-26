c36064acf81e0875dcc079013b4d5898c2356907
jjh319 <136777270+jjh319@users.noreply.github.com>
2023-12-26 12:42:24 +0900

    java편 배열 학습

---
+++ array_04/src/array/Array01.java
@@ -0,0 +1,25 @@
+package array;
+
+
+public class Array01 {
+
+	
+	public static void main(String[] args) {
+		
+		int[] ar;
+		ar = new int[3];
+		
+		ar[0] = 25;
+		ar[1] = 43;
+		ar[2] = 30;
+//		ar[3] = 27;
+//		ar[4] = 52;
+		
+		for(int i= 0; i<3; i++) {
+		System.out.println("배열명 ar = " + ar[i]);
+		
+		} // for
+		
+	} // main
+
+} // end class
